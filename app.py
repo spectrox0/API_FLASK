65a3d3353b8b320f538e33b77df4292290baf849
Aleja <alejanvelazco2008@hotmail.com>
2021-01-13 00:18:18 -0400

    first endpoint

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 #from apis.covid import covid
 from flask_restful import Api
 from routes import initialize_routes
-
+from flask_cors import CORS
 #.register_blueprint(covid)
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
+
 
 #@app.route('/')
 #def hello_world():
